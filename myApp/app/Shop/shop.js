c4eac91a7b810b1365108545d164687e38610037
Vagrant Default User <vagrant@debian-jessie.vagrantup.com>
2016-03-10 10:09:00 +0000

    [FEATURE] Add Bootstrap and Material2

---
+++ myApp/app/Shop/shop.js
@@ -0,0 +1,16 @@
+System.register([], function(exports_1) {
+    var Shop;
+    return {
+        setters:[],
+        execute: function() {
+            Shop = (function () {
+                function Shop(id) {
+                    this.id = id;
+                }
+                return Shop;
+            })();
+            exports_1("Shop", Shop);
+        }
+    }
+});
+//# sourceMappingURL=shop.js.map
