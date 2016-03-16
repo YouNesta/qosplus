11896d14984a70160e53bfebc3dd8dea29c878da
Vagrant Default User <vagrant@debian-jessie.vagrantup.com>
2016-03-16 03:07:47 +0000

    [FEATURE] Add User Dashboard

---
+++ myApp/app/User/user-dashboard.component.ts
@@ -0,0 +1,17 @@
+import {Component} from 'angular2/core';
+
+
+import {tokenNotExpired} from 'angular2-jwt';
+import {CanActivate} from "angular2/router";
+@CanActivate(() => tokenNotExpired('token'))
+
+@Component({
+    templateUrl: "app/User/user-dashboard.html",
+})
+
+
+
+export class UserDashBoardComponent {
+
+}
+
