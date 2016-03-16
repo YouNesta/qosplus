46c4cc8449612f1a9873eeec61efbc086db27230
Vagrant Default User <vagrant@debian-jessie.vagrantup.com>
2016-03-16 03:06:59 +0000

    [FEATURE] Add Admin Dashboard

---
+++ myApp/app/Admin/admin-dashboard.component.ts
@@ -0,0 +1,23 @@
+import {Component} from 'angular2/core';
+
+import {
+    RouteConfig,
+    ROUTER_DIRECTIVES,
+    ROUTER_PROVIDERS,
+} from 'angular2/router';
+import {AdminSubscribeComponent} from "./admin-subscribe.component";
+import {tokenNotExpired} from 'angular2-jwt';
+import {CanActivate} from "angular2/router";
+@CanActivate(() => tokenNotExpired('token'))
+
+
+@Component({
+    templateUrl: "app/Admin/admin-dashboard.html",
+})
+
+
+
+export class AdminDashBoardComponent {
+
+}
+
