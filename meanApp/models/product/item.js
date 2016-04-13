f28a18607a5c633763dd5c18245460f82f70eb29
younesboulkaddid <boulkaddid.younes@gmail.com>
2016-04-13 04:27:51 +0000

    [FIX] Product List for new product structure

--- meanApp/models/product/item.js
+++ meanApp/models/product/item.js
@@ -28,10 +28,6 @@ var ItemSchema = new mongoose.Schema({
     condition: {
         type: Number,
         required : true
-    },
-    stock: {
-        type: Number,
-        required : true
     }
 });
 
