0415a04f6b7ab0708acde138560913356951d0de
younesboulkaddid <boulkaddid.younes@gmail.com>
2016-04-14 04:00:25 +0000

    [FEATURE] Update port live-server

--- myApp/app.js
+++ myApp/app.js
@@ -4,7 +4,7 @@
 liveServer = require("live-server");
 
 var params = {
-    port: 8080, // Set the server port. Defaults to 8080.
+    port: 8181, // Set the server port. Defaults to 8080.
     host: "0.0.0.0", // Set the address to bind to. Defaults to 0.0.0.0.
     root: "./", // Set root directory that's being server. Defaults to cwd.
     open: false, // When false, it won't load your browser by default.
