ea55ae4b8612f6d492a04eb9f711a0c38df4b2d3
medrupaloscil <baptistefolio@yahoo.fr>
2016-04-18 02:53:54 +0200

    [FEATURE] Add front design

--- myApp/app/Layouts/nav.component.ts
+++ myApp/app/Layouts/nav.component.ts
@@ -5,23 +5,12 @@ import {ROUTER_DIRECTIVES} from "angular2/router";
 
 @Component({
     selector: "nav",
-<<<<<<< HEAD
     template:
     "<li><a [routerLink]=\"['User', 'Login']\">Inscription / Connexion</a></li>" +
     "<li><a [routerLink]=\"['Home', 'Products']\">Nos Produits</a></li>" +
     "<li><a [routerLink]=\"['Home', 'Contact']\">Nos Engagements</a></li>" +
     "<li><a [routerLink]=\"['Home', 'About']\">Qui sommes nous ?</a></li>" +
     "<li><a [routerLink]=\"['Home', 'Homepage']\">Accueil</a></li>",
-=======
-    template: "<li><a [routerLink]=\"['Home', 'Homepage']\">Home</a></li>" +
-    "<li><a [routerLink]=\"['Home', 'About']\">À propos de nous</a></li>" +
-/*
-    "<li><a [routerLink]=\"['Product', 'List']\">Nos Produits</a></li>" +
-*/
-    "<li><a [routerLink]=\"['Home', 'Contact']\">Contact</a></li>" +
-    "<li><a [routerLink]=\"['User', 'Login']\">Se connecter</a></li>" +
-    "<li><a [routerLink]=\"['User', 'Subscribe']\">S'inscrire</a></li>",
->>>>>>> origin/dev
     directives: [ROUTER_DIRECTIVES]
 })
 
