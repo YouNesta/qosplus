79a3a3bcd4ff3f606da1ca764e95cfd60f015201
younesboulkaddid <boulkaddid.younes@gmail.com>
2016-04-20 06:16:30 +0200

    [FIX] Update Log function for folder conflict in prod

--- meanApp/modules/logs.js
+++ meanApp/modules/logs.js
@@ -7,31 +7,7 @@ var logger = require('winston');
 
 module.exports = {
     logFolder: function(){
-        var dir = this.mkdir();
         var y = new Date();
-        logger.add(logger.transports.File, { filename: dir+"/"+y.getHours()+'H.log' });
-    },
-    mkdir: function(){
-        var dir = './logs';
-        var y = new Date();
-        var param = [y.getFullYear(), y.getMonth(),y.getDate()];
-        var lastItem = null;
-        param.forEach(function(item, e) {
-            if(lastItem == param[e - 1]){
-                dir+= "/"+item;
-                if (!fs.existsSync(dir)){
-                    fs.mkdirSync(dir);
-                }
-            }else{
-                if (!fs.existsSync(dir)){
-                    fs.mkdirSync(dir);
-                }
-            }
-            lastItem = item;
-        });
-
-        return dir;
+        logger.add(logger.transports.File, { filename: ".logs"+"/"+y.getFullYear()+y.getMonth()+y.getDay()+"-"+y.getHours+'H.log' });
     }
-
-
 }
