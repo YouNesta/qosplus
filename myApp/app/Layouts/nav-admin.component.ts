238da0e6629587aad74c288f44b6477fd1516baf
younesboulkaddid <boulkaddid.younes@gmail.com>
2016-05-04 04:39:13 +0200

    [FIX] Delete require useless

--- myApp/app/Layouts/nav-admin.component.ts
+++ myApp/app/Layouts/nav-admin.component.ts
@@ -1,8 +1,6 @@
 import {Component} from 'angular2/core';
 import {ROUTER_DIRECTIVES} from "angular2/router";
-import {OnInit} from "angular2/core";
 import {UserFactory} from "../User/user.factory";
-import {Input} from "angular2/core";
 import {Router} from "angular2/router";
 
 
@@ -16,7 +14,7 @@ import {Router} from "angular2/router";
 export class NavAdminComponent  {
 
 
-    constructor(public service: UserFactory, public router: Router){
+    constructor(public router: Router){
         this.title = "QosPlus";
     }
 
