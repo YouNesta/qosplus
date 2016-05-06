4fa7d7c846baea0d40fe8c6acfadcc1d7810c27e
ChazalFlorian <chazal.florain@gmail.com>
2016-05-06 14:27:29 +0200

    Products display V1

---
+++ myApp/app/User/user-product-display.component.ts
@@ -0,0 +1,35 @@
+import {Component, Input, Output, EventEmitter} from 'angular2/core';
+import {ProductFactory} from "../Product/product.factory";
+import {MODAL_DIRECTIVES} from "ng2-bs3-modal";
+import {ACCORDION_DIRECTIVES} from "ng2-bootstrap";
+import {TagInputComponent} from "angular2-tag-input";
+
+
+@Component({
+    providers: [],
+    templateUrl: "app/User/user-products-list.html",
+    directives: [ ACCORDION_DIRECTIVES, MODAL_DIRECTIVES]
+})
+
+export class UserProductsListComponent {
+    products: Object;
+    product: Object;
+
+    constructor(public service: ProductFactory){
+        this.service.getOneProduct(product)
+            .subscribe(
+                response => {
+                    if(response.success){
+                        this.products = response.data;
+                        console.log(this.products)
+                    }else{
+                        console.log(response);
+                    }
+                },
+                err =>  console.log(err),
+                () => console.log('get product list Complete')
+            );
+    }
+
+}
+
