c2d9009fdeaa3576ba18019f70a2d26a7be21faf
ChazalFlorian <chazal.florain@gmail.com>
2016-05-16 08:53:09 +0200

    [FIX]front design review

--- myApp/app/Home/home-products.component.ts
+++ myApp/app/Home/home-products.component.ts
@@ -1,11 +1,38 @@
 import {Component} from 'angular2/core';
+import {ProductFactory} from "../Product/product.factory";
 
 
 @Component({
-    templateUrl: "app/Home/home-products.html"
+    selector: "test",
+    templateUrl: "app/Home/home-products.html",
+    providers:[ProductFactory]
 })
 
 export class HomeProductsComponent {
+    products: Object;
+
+
+    constructor(public service: ProductFactory){
+
+    }
+
+    getProducts(){
+        this.service.getProduct()
+            .subscribe(
+                response => {
+                    if(response.success){
+                        this.products = response.data;
+                        for(var i in this.products){
+                            this.isOpen.push(false);
+                        }
+                    }else{
+                        console.log(response);
+                    }
+                },
+                err =>  console.log(err),
+                () => console.log('get product list Complete')
+            );
+    }
 
 }
 
