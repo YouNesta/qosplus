6ecb6807d9b67aa9f5693f940ada8936599875c1
ChazalFlorian <chazal.florian@gmail.com>
2016-06-17 16:52:59 +0200

    [Fix] userDashboard: shop opening display

--- myApp/app/User/user-dashboard.component.ts
+++ myApp/app/User/user-dashboard.component.ts
@@ -29,8 +29,151 @@ export class UserDashboardComponent {
     };
 
     shops = [
-        { "_id" : "56e9eb8076b8f3a707192676", "name" : "Younesta", "socialReason" : "YOUNESTA SARL", "adress" : "43 rue de malabry", "adress2" : "", "city" : "Maisse", "zipCode" : 91720, "mobile" : "06 50 90 12 05", "phone" : "01 60 78 37 94", "fax" : "01 60 78 37 94", "mail" : "younes.boulkaddid@supinternet.fr", "tva" : 0.9, "siret" : 987654567890987, "adeli" : 876545678987654, "nightBox" : true, "transporteur" : "Mathieu", "openDay" : "Lun", "closeDay" : "Lun", "openHour" : "1970-01-01T00:00:00Z", "closeHour" : "1970-01-01T00:00:00Z", "__v" : 0 },
-        { "_id" : "56e9eb8076b8f3a707192678", "name" : "Younesta", "socialReason" : "YOUNESTA SARL", "adress" : "43 rue de malabry", "adress2" : "", "city" : "Maisse", "zipCode" : 91720, "mobile" : "06 50 90 12 05", "phone" : "01 60 78 37 94", "fax" : "01 60 78 37 94", "mail" : "younes.boulkaddid@supinternet.fr", "tva" : 0.9, "siret" : 987654567890987, "adeli" : 876545678987654, "nightBox" : true, "transporteur" : "Mathieu", "openDay" : "Lun", "closeDay" : "Lun", "openHour" : "1970-01-01T00:00:00Z", "closeHour" : "1970-01-01T00:00:00Z", "__v" : 0 }
+        { "_id" : "56e9eb8076b8f3a707192676", "name" : "Younesta", "socialReason" : "YOUNESTA SARL", "adress" : "43 rue de malabry", "adress2" : "", "city" : "Maisse", "zipCode" : 91720, "mobile" : "06 50 90 12 05", "phone" : "01 60 78 37 94", "fax" : "01 60 78 37 94", "mail" : "younes.boulkaddid@supinternet.fr", "tva" : 0.9, "siret" : 987654567890987, "adeli" : 876545678987654, "nightBox" : true, "transporteur" : "Mathieu",
+            disponibility: [{
+                day: "Lundi",
+                data: {
+                    morning: {
+                        opening: new Date(),
+                        closing: new Date()
+                    },
+                    afternoon: {
+                        opening: new Date(),
+                        closing: new Date()
+                    }
+                }
+            },
+                {
+                    day: "Mardi",
+                    data: {
+                        morning: {
+                            opening: new Date(),
+                            closing: new Date()
+                        },
+                        afternoon: {
+                            opening: new Date(),
+                            closing: new Date()
+                        }
+                    }
+                },
+                {
+                    day: "Mercredi",
+                    data: {
+                        morning: {
+                            opening: new Date(),
+                            closing: new Date()
+                        },
+                        afternoon: {
+                            opening: new Date(),
+                            closing: new Date()
+                        }
+                    }
+                },
+                {
+                    day: "Jeudi",
+                    data: {
+                        morning: {
+                            opening: new Date(),
+                            closing: new Date()
+                        },
+                        afternoon: {
+                            opening: new Date(),
+                            closing: new Date()
+                        }
+                    }
+                },
+                {
+                    day: "Vendredi",
+                    data: {
+                        morning: {
+                            opening: new Date(),
+                            closing: new Date()
+                        },
+                        afternoon: {
+                            opening: new Date(),
+                            closing: new Date()
+                        }
+                    }
+                }],
+            "__v" : 0 },
+        { "_id" : "56e9eb8076b8f3a707192678", "name" : "Younesta", "socialReason" : "YOUNESTA SARL", "adress" : "43 rue de malabry", "adress2" : "", "city" : "Maisse", "zipCode" : 91720, "mobile" : "06 50 90 12 05", "phone" : "01 60 78 37 94", "fax" : "01 60 78 37 94", "mail" : "younes.boulkaddid@supinternet.fr", "tva" : 0.9, "siret" : 987654567890987, "adeli" : 876545678987654, "nightBox" : true, "transporteur" : "Mathieu", "openDay" : "Lun", "closeDay" : "Lun", "openHour" : "1970-01-01T00:00:00Z", "closeHour" : "1970-01-01T00:00:00Z", disponibility: [{
+            day: "Lundi",
+            data: {
+                morning: {
+                    opening: new Date(),
+                    closing: new Date()
+                },
+                afternoon: {
+                    opening: new Date(),
+                    closing: new Date()
+                }
+            }
+        },
+            {
+                day: "Mardi",
+                data: {
+                    morning: {
+                        opening: new Date(),
+                        closing: new Date()
+                    },
+                    afternoon: {
+                        opening: new Date(),
+                        closing: new Date()
+                    }
+                }
+            },
+            {
+                day: "Mercredi",
+                data: {
+                    morning: {
+                        opening: new Date(),
+                        closing: new Date()
+                    },
+                    afternoon: {
+                        opening: new Date(),
+                        closing: new Date()
+                    }
+                }
+            },
+            {
+                day: "Jeudi",
+                data: {
+                    morning: {
+                        opening: new Date(),
+                        closing: new Date()
+                    },
+                    afternoon: {
+                        opening: new Date(),
+                        closing: new Date()
+                    }
+                }
+            },
+            {
+                day: "Vendredi",
+                data: {
+                    morning: {
+                        opening: new Date(),
+                        closing: new Date()
+                    },
+                    afternoon: {
+                        opening: new Date(),
+                        closing: new Date()
+                    }
+                }
+            },
+            {
+                day: "Samedi",
+                data: {
+                    morning: {
+                        opening: new Date(),
+                        closing: new Date()
+                    },
+                    afternoon: {
+                        opening: new Date(),
+                        closing: new Date()
+                    }
+                }
+            }], "__v" : 0 }
     ];
 
     commands = [];
