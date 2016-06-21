024df8467a52efe096f03d6129402c2f650d29ff
medrupaloscil <baptistefolio@yahoo.fr>
2016-06-22 01:38:19 +0200

    [Features] Only display user price

--- myApp/app/User/Product/products-list.component.ts
+++ myApp/app/User/Product/products-list.component.ts
@@ -3,6 +3,7 @@ import {ProductFactory} from "../../Product/product.factory";
 import {MODAL_DIRECTIVES} from "ng2-bs3-modal";
 import {ProductAddCartComponent} from "../../Product/product-add-cart.component";
 import {TagInputComponent} from "angular2-tag-input";
+import {UserFactory} from "../user.factory";
 
 
 @Component({
@@ -15,11 +16,27 @@ export class UserProductsListComponent {
     products: Object ;
     productCart: String;
     isOpen = [];
+    user = {};
+    type = 0;
 
+    constructor(public service: ProductFactory, public userService: UserFactory){
 
+        var user = JSON.parse(localStorage.getItem("user"));
 
-    constructor(public service: ProductFactory){
-        this.getProducts();
+        userService.getUserById(user._id)
+            .subscribe(
+                response => {
+                    if(response.success){
+                        this.user = response.data;
+                        this.type = this.user.type.type;
+                        this.getProducts();
+                    }else{
+                        console.log(response);
+                    }
+                },
+                err =>  console.log(err),
+                () => console.log('user get')
+            );
     }
 
     loadModalProduct(product, modal) {
@@ -35,6 +52,9 @@ export class UserProductsListComponent {
                         this.products = response.data;
                         for(var i in this.products){
                             this.isOpen.push(false);
+                            if (this.user.role == 0) {
+                                this.products[i].price = [this.products[i].price[this.type]];
+                            }
                         }
                     }else{
                         console.log(response);
